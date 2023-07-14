45b4ddb29a52d3b2e0ffbc624e825bb6cf901e07
Ram Dular <rammey115@gmail.com>
2023-07-14 14:50:46 +0545

    machine Learning and EDA

---
+++ ml.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+df = pd.read_csv("D:\Excel file\Sugarcane_Production.csv")
+print(df)
+
+df.head(5)
